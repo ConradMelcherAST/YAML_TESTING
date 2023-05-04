b4983df595bd421d2531374cdceb1d239234b44f
Conrad Melcher <conrad.melcher@asttechlabs.com>
2023-05-04 17:42:19 -0400

    Reset the head and made linting changes

--- src/conradmath.py
+++ src/conradmath.py
@@ -1,17 +1,26 @@
+"""Module YAML_TESTING.src
+Basic arithmatic functions
+"""
 
-class Calculator(object):
+
+class Calculator():
+    """Calculator class"""
 
     def __init__(self):
         self._last_answser = 0.0
 
-    def cadd(self, x, y):
-        return x + y
-    
-    def csubtract(self, x, y):
-        return x - y
-    
-    def cmultiply(self, x, y):
-        return x * y
-    
-    def cdivide(self, x, y):
-        return x / y
+    def cadd(self, first_number, second_number):
+        """Function to add two numbers"""
+        return first_number + second_number
+
+    def csubtract(self, first_number, second_number):
+        """Function to subtract one number from another"""
+        return first_number - second_number
+
+    def cmultiply(self, first_number, second_number):
+        """Function to multiply two numbers"""
+        return first_number * second_number
+
+    def cdivide(self, first_number, second_number):
+        """Function to divide one number by another"""
+        return first_number / second_number
